1a205ef096d1d356cf190fbc2759715429e068bb
李松达 <lisda@yonyou.com>
2016-07-07 13:21:51 +0800

    82.Remove Duplicates from Sorted List II

--- src/com/brooks/LC_Problem_82.java
+++ src/com/brooks/LC_Problem_82.java
@@ -6,4 +6,46 @@ package com.brooks;
  * @description:
  */
 public class LC_Problem_82{
+    public ListNode deleteDuplicates(ListNode head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode pre=dummy;
+        ListNode cur=head;
+        ListNode next=head.next;
+        boolean flag=false;
+        while(next!=null){
+            if(next.val==cur.val){
+                flag=true;
+                next=next.next;
+                cur.next=next;
+            }else{
+                if(flag){
+                    pre.next=next;
+                    cur=next;
+                    next=next.next;
+                    flag=false;
+                }else{
+                    pre=cur;
+                    cur=next;
+                    next=next.next;
+                }
+            }
+        }
+        if(flag){
+            pre.next=null;
+        }
+        return dummy.next;
+    }
+
+    static class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int x){
+            val=x;
+        }
+    }
 }
