e09692f259546439abb430756e609dd91e12392d
Brooks <lisongda0129@outlook.com>
2016-07-07 09:58:17 +0800

    81	.Search in Rotated Sorted Array II

--- src/com/brooks/LC_Problem_81.java
+++ src/com/brooks/LC_Problem_81.java
@@ -1,5 +1,5 @@
 ﻿package com.brooks;
-<<<<<<< HEAD
+
 
 /**
  * @author: 李松达
@@ -38,39 +38,4 @@ public class LC_Problem_81{
         return nums[left]==target;
     }
 }
-=======
-/**
- * @author: 李松达
- * @date: 2016/7/6.
- */
-public class LC_Problem_80{
-    public static void main(String[] args){
-        LC_Problem_80 lc_problem_80=new LC_Problem_80();
-        int removeDuplicates=lc_problem_80.removeDuplicates(new int[]{1,1,1,2,3,3,3,4});
-        System.out.println(removeDuplicates);
-    }
-    public int removeDuplicates(int[] nums){
-        if(nums==null||nums.length==0){
-            return 0;
-        }
-        int len=0;
-        int ele=nums[0];
-        int time=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==ele){
-                if(time<2){
-                    nums[len++]=nums[i];
-                    time++;
-                }else {
-                    time++;
-                }
-            }else{
-                ele=nums[i];
-                nums[len++]=nums[i];
-                time=1;
-            }
-        }
-        return len;
-    }
-}
->>>>>>> edeae9a381bbefedf9b0bc4f9ffe5347bb66a661
+
