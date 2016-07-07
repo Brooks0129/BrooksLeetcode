5c104a4062804486a3ae1823a48f68e87a6171ce
Brooks0129 <lisongda0129@outlook.com>
2016-07-07 22:17:44 +0800

    87. Scramble String

---
+++ src/com/brooks/LC_Problem_87.java
@@ -0,0 +1,36 @@
+package com.brooks;
+/**
+ * @author: 李松达
+ * @date: 2016/7/7.
+ */
+public class LC_Problem_87{
+    public boolean isScramble(String s1,String s2){
+        if(s1.equals(s2)){
+            return true;
+        }
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        int[] chars=new int[26];
+        for(int i=0;i<s1.length();i++){
+            chars[s1.charAt(i)-'a']++;
+            chars[s2.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(chars[i]!=0){
+                return false;
+            }
+        }
+        for(int i=1;i<s1.length();i++){
+            if(isScramble(
+                    s1.substring(0,i),s2.substring(0,i))
+                    &&isScramble(s1.substring(i),s2.substring(i)))
+                return true;
+            if(isScramble(
+                    s1.substring(0,i),s2.substring(s2.length()-i))
+                    &&isScramble(s1.substring(i),s2.substring(0,s2.length()-i)))
+                return true;
+        }
+        return false;
+    }
+}
