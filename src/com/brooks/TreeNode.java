eb4e98b5300abbe446690395eca12c2094a62a49
Brooks0129 <lisongda0129@outlook.com>
2016-07-08 17:07:02 +0800

    TreeNode

---
+++ src/com/brooks/TreeNode.java
@@ -0,0 +1,13 @@
+package com.brooks;
+/**
+ * @author: 李松达
+ * @date: 2016/7/8.
+ */
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val=x;
+    }
+}
