ae4b63dcd6ca9a04052dc22da4ac45b0d3448539
Brooks0129 <lisongda0129@outlook.com>
2016-07-10 20:31:22 +0800

    10. Regular Expression Matching

--- src/com/brooks/LC_Problem_1.java
+++ src/com/brooks/LC_Problem_1.java
@@ -1,7 +1,26 @@
 package com.brooks;
+import java.util.HashMap;
 /**
  * @author: 李松达
  * @date: 2016/7/10.
  */
 public class LC_Problem_1{
+    public int[] twoSum(int[] nums, int target) {
+        int[] res=new int[2];
+        if(nums==null||nums.length<2){
+            return res;
+        }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                res[0]=map.get(target-nums[i]);
+                res[1]=i;
+                return res;
+            }else{
+                map.put(nums[i],i);
+            }
+
+        }
+        return res;
+    }
 }
