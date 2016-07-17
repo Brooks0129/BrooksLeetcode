99c83ec70aeecd761928f7a11f4ad2a78e855f2c
Brooks0129 <lisongda0129@outlook.com>
2016-07-17 15:40:23 +0800

    137. Single Number II

--- src/com/brooks/LC_Problem_137.java
+++ src/com/brooks/LC_Problem_137.java
@@ -4,12 +4,7 @@ package com.brooks;
  * @date: 2016/7/17.
  */
 public class LC_Problem_137{
-    public static void main(String[] args){
-        int[] nums=new int[]{9,9,9};
-        int i=singleNumber(nums);
-        System.out.println(i);
-    }
-    public static int singleNumber(int[] nums){
+    public  int singleNumber(int[] nums){
             int ones = 0, twos = 0;
             for(int i = 0; i < nums.length; i++){
                 ones = (ones ^ nums[i]) & ~twos;
