cb460260a09628987b9d767640e1fd15065a047f
Brooks0129 <lisongda0129@outlook.com>
2016-07-26 11:33:00 +0800

    169. Majority Element

---
+++ src/com/brooks/LC_Problem_169.java
@@ -0,0 +1,22 @@
+package com.brooks;
+/**
+ * @author: 李松达
+ * @date: 2016/7/26.
+ */
+public class LC_Problem_169{
+    public int majorityElement(int[] nums){
+        int major=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(count==0){
+                major=nums[i];
+                count++;
+            }else if(nums[i]==major){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return major;
+    }
+}
