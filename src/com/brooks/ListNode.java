5e12e1267d576644d36ab8bb5e5c95c0b4221b9f
Brooks0129 <lisongda0129@outlook.com>
2016-07-28 18:27:10 +0800

    ListNode

---
+++ src/com/brooks/ListNode.java
@@ -0,0 +1,14 @@
+package com.brooks;
+/**
+ * @author: 李松达
+ * @date: 2016/7/16.
+ */
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val=x;
+        next=null;
+    }
+}
+
