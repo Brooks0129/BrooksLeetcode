3eed291250f5a727d25b201e9d7b0d54006a2c03
Brooks0129 <lisongda0129@outlook.com>
2016-08-07 15:00:49 +0800

    240. Search a 2D Matrix II

---
+++ src/com/brooks/LC_Problem_240.java
@@ -0,0 +1,24 @@
+package com.brooks;
+/**
+ * @author: 李松达
+ * @date: 2016/8/7.
+ */
+public class LC_Problem_240{
+    public boolean searchMatrix(int[][] matrix,int target){
+        if(matrix==null||matrix.length<1||matrix[0].length<1){
+            return false;
+        }
+        int col=matrix[0].length-1;
+        int row=0;
+        while(col>=0&&row<=matrix.length-1){
+            if(target==matrix[row][col]){
+                return true;
+            }else if(target<matrix[row][col]){
+                col--;
+            }else if(target>matrix[row][col]){
+                row++;
+            }
+        }
+        return false;
+    }
+}
